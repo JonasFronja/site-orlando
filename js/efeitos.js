e1312bcafa102824452505d00f65693ce0b033f7
Jonas Fronja <jonasfronja@hotmail.com>
2018-01-16 20:39:26 -0200

    versao 1.4

--- js/efeitos.js
+++ js/efeitos.js
@@ -41,5 +41,17 @@ $(document).ready(function(){
 
 				});
 
+				var owl = $(".thumbnails").data('owlCarousel');
+
+				$('#btn-news-prev').on("click",function(){
+
+					owl.prev();
+				});
+
+				$('#btn-news-next').on("click",function(){
+
+					owl.next();
+				});
+
 				
 			});
