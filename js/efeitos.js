a3c8522615c060189461cca7520d7eee902ebabb
Jonas Fronja <jonasfronja@hotmail.com>
2018-01-23 17:33:17 -0200

    versao 1.6

--- js/efeitos.js
+++ js/efeitos.js
@@ -53,5 +53,22 @@ $(document).ready(function(){
 					owl.next();
 				});
 
+
+				$("#btn-bars").on("click",function(){
+
+					$("header").toggleClass("open-menu");
+				});
+
+				$("#menu-mobile-mask, .btn-close").on("click",function(){
+
+					$("header").removeClass("open-menu");
+				});
+
+				$("#btn-search").on("click",function(){
+
+					$("header").toggleClass("open-search");
+					$("#input-search-mobile").focus();
+				});
+
 				
 			});
